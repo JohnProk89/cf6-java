850091392a9e1c5379b31389b3b431545b018a9a
Giannis Prokopiou <johnprok89@gmail.com>
2025-02-18 22:40:28 +0200

    OOP 1st lesson, chapter 12

---
+++ src/gr/aueb/cf/ch12/MainCustomer.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.ch12;
+
+import gr.aueb.cf.ch12.model.Customer;
+
+public class MainCustomer {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        customer.setId(1);
+        customer.setVatRegistrationNo("128458231");
+
+        Customer anna = new Customer(2, "Anna", "G.", "234567890", "6999999999", "Attica",
+                "Central Attica", "Athens", "Pattission", "76", "10434");
+
+        System.out.println("Customer ΑΦΜ: " + customer.getVatRegistrationNo());
+        System.out.println("Anna's vat: " + anna.getVatRegistrationNo());
+
+    }
+
+}
