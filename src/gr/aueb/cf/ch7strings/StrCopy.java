b81b274caa343d17e850634bf3e46abbe1c1a0d5
Giannis Prokopiou <johnprok89@gmail.com>
2025-03-11 18:48:27 +0200

    comment fix

--- src/gr/aueb/cf/ch7strings/StrCopy.java
+++ src/gr/aueb/cf/ch7strings/StrCopy.java
@@ -7,7 +7,7 @@ public class StrCopy {
 
     public static void main(String[] args) {
         String s1 = "Athens";
-        String s2 = copyStr(s1); // Not indicate as redundant by IDE
+        String s2 = copyStr(s1); // Not indicated as redundant by IDE
         String s3 = s1;         // Indicated as redundant by IDE
 
         System.out.println(s2);
